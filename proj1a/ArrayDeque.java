98d0cad9eb4efe1b15a0d446f034d9f5da6d97da
Yjm <19307130191@fudan.edu.cn>
2021-08-10 20:23:06 +0800

    committed

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -6,16 +6,16 @@ public class ArrayDeque<T> {
     private int lastPointer;
 
     /** Creates an empty array deque. **/
-    public ArrayDeque(){
+    public ArrayDeque() {
         size = 0;
         items = (T[]) new Object[8];
         firstPointer = 0;
         lastPointer = 0;
     }
 
-    private void resizing(int capacity){
+    private void resizing(int capacity) {
         T[] newItems = (T[]) new Object[capacity];
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++){
             newItems[i] = items[(firstPointer + i) % items.length];
         }
         this.items = newItems;
@@ -24,8 +24,8 @@ public class ArrayDeque<T> {
     }
 
     /** Adds an item of type T to the front of the deque. **/
-    public void addFirst(T item){
-        if(size == items.length){
+    public void addFirst(T item) {
+        if (size == items.length){
             resizing(2 * size);
         }
         size++;
@@ -34,8 +34,8 @@ public class ArrayDeque<T> {
     }
 
     /** Adds an item of type T to the back of the deque. **/
-    public void addLast(T item){
-        if(size == items.length){
+    public void addLast(T item) {
+        if (size == items.length){
             resizing(2 * size);
         }
         size++;
@@ -44,19 +44,19 @@ public class ArrayDeque<T> {
     }
 
     /** Returns true if deque is empty, false otherwise. **/
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
     /** Returns the number of items in the deque. **/
-    public int size(){
+    public int size() {
         return size;
     }
 
     /** Prints the items in the deque from first to last, separated by a space. **/
-    public void printDeque(){
+    public void printDeque() {
         System.out.print("[ ");
-        for(int i = firstPointer; i < size; i++){
+        for (int i = firstPointer; i < size; i++){
             System.out.print(items[i] + " ");
         }
         System.out.print("]");
@@ -64,31 +64,30 @@ public class ArrayDeque<T> {
     }
 
     /** Removes and returns the item at the front of the deque. If no such item exists, returns null. **/
-    public T removeFirst(){
-        if(size == 0){
+    public T removeFirst() {
+        if (size == 0){
             return null;
         }
         size--;
-        if(items.length > 16 && ((double)size / items.length) <= 0.25){
-            resizing(items.length / 2);
-        }
         T result = items[firstPointer];
         firstPointer = (firstPointer + 1) % items.length;
+        if (items.length > 16 && ((double)size / items.length) < 0.25){
+            resizing(items.length / 2);
+        }
         return result;
     }
 
     /** Removes and returns the item at the back of the deque. If no such item exists, returns null. **/
-    public T removeLast(){
-        if(size == 0){
+    public T removeLast() {
+        if (size == 0){
             return null;
         }
         size--;
-
-        if(items.length > 16 && ((double)size / items.length) <= 0.25){
-            resizing(items.length / 2);
-        }
         lastPointer = (lastPointer - 1 + items.length) % items.length;
         T result = items[lastPointer];
+        if (items.length > 16 && ((double)size / items.length) < 0.25){
+            resizing(items.length / 2);
+        }
         return result;
     }
 
@@ -96,8 +95,8 @@ public class ArrayDeque<T> {
      * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
      * If no such item exists, returns null. Must not alter the deque!
      */
-    public T get(int index){
-        if(index < 0 || index >= size){
+    public T get(int index) {
+        if (index < 0 || index >= size){
             return null;
         }
         return items[(firstPointer + index) % items.length];
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,11 +1,11 @@
 public class LinkedListDeque<T> {
 
-    private class Node<T>{
+    private class Node<T> {
         Node prev;
         T value;
         Node next;
 
-        Node(Node p, T v, Node n){
+        Node(Node p, T v, Node n) {
             this.prev = p;
             this.value = v;
             this.next = n;
@@ -16,7 +16,7 @@ public class LinkedListDeque<T> {
     private Node sentinel;
 
     /** Creates an empty LinkedListDeque **/
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         size = 0;
         sentinel = new Node(null, (T)null, null);
         sentinel.prev = sentinel;
@@ -24,7 +24,7 @@ public class LinkedListDeque<T> {
     }
 
     /** Adds an item of type T to the front of the deque. **/
-    public void addFirst(T item){
+    public void addFirst(T item) {
         size++;
         Node newFirst = new Node(sentinel, item, sentinel.next);
         sentinel.next.prev = newFirst;
@@ -32,7 +32,7 @@ public class LinkedListDeque<T> {
     }
 
     /** Adds an item of type T to the back of the deque. **/
-    public void addLast(T item){
+    public void addLast(T item) {
         size++;
         Node newLast = new Node(sentinel.prev, item, sentinel);
         sentinel.prev.next = newLast;
@@ -40,20 +40,20 @@ public class LinkedListDeque<T> {
     }
 
     /** Returns true if deque is empty, false otherwise. **/
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
     /** Returns the number of items in the deque. **/
-    public int size(){
+    public int size() {
         return size;
     }
 
     /** Prints the items in the deque from first to last, separated by a space. **/
-    public void printDeque(){
+    public void printDeque() {
         System.out.print("[ ");
         Node p = sentinel.next;
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++){
             System.out.print(p.value + " ");
             p = p.next;
         }
@@ -62,8 +62,8 @@ public class LinkedListDeque<T> {
     }
 
     /** Removes and returns the item at the front of the deque. If no such item exists, returns null. **/
-    public T removeFirst(){
-        if(size == 0){
+    public T removeFirst() {
+        if (size == 0){
             return null;
         }
         size--;
@@ -74,8 +74,8 @@ public class LinkedListDeque<T> {
     }
 
     /** Removes and returns the item at the back of the deque. If no such item exists, returns null. **/
-    public T removeLast(){
-        if(size == 0){
+    public T removeLast() {
+        if (size == 0){
             return null;
         }
         size--;
@@ -89,30 +89,30 @@ public class LinkedListDeque<T> {
      * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
      * If no such item exists, returns null. Must not alter the deque!
      */
-    public T get(int index){
-        if(index < 0 || index >= size){
+    public T get(int index) {
+        if (index < 0 || index >= size){
             return null;
         }
         Node p = sentinel.next;
-        for(int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++){
             p = p.next;
         }
         return (T)p.value;
     }
 
-    private T getRecursiveHelpMethod(Node p, int index){
-        if(index == 0){
+    private T getRecursiveHelpMethod(Node p, int index) {
+        if (index == 0){
             return (T)p.value;
         }
         return (T)getRecursiveHelpMethod(p.next, index - 1);
     }
 
     /** Same as get, but uses recursion. **/
-    public T getRecursive(int index){
-        if(index < 0 || index >= size){
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size){
             return null;
         }
-        return (T)getRecursiveHelpMethod(sentinel.next, index);
+        return (T) getRecursiveHelpMethod(sentinel.next, index);
     }
 
 }
