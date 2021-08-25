654646a9554b5639d5b0dcdc52105b3eb2ed0af2
Yjm <19307130191@fudan.edu.cn>
2021-08-25 15:56:39 +0800

    committed

---
+++ proj1b/TestOffByOne.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestOffByOne {
+
+    // You must use this CharacterComparator and not instantiate
+    // new ones, or the autograder might be upset.
+    static CharacterComparator offByOne = new OffByOne();
+
+    // Your tests go here.
+    @Test
+    public void test1() {
+        assertTrue(offByOne.equalChars('a', 'b'));
+    }
+}
