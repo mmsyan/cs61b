14295db0dbad5800e30bce0279fb1bce7cd1cba1
mmsyan <88571295+mmsyan@users.noreply.github.com>
2021-09-08 10:36:27 +0800

    Add files via upload

---
+++ hw3/hash/OomageTestUtility.java
@@ -0,0 +1,23 @@
+package hash;
+
+import java.util.List;
+
+public class OomageTestUtility {
+    public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
+        /*
+         * Write a utility function that returns true if the given oomages
+         * have hashCodes that would distribute them fairly evenly across
+         * M buckets. To do this, convert each oomage's hashcode in the
+         * same way as in the visualizer, i.e. (& 0x7FFFFFFF) % M.
+         * and ensure that no bucket has fewer than N / 50
+         * Oomages and no bucket has more than N / 2.5 Oomages.
+         */
+        int N = oomages.size();
+        for (Oomage oomage : oomages) {
+            int bucketNum = (oomage.hashCode() & 0x7FFFFFFF) % M;
+            if ((bucketNum < (double) M / 50) || (bucketNum > M / 2.5))
+                return false;
+        }
+        return true;
+    }
+}
