adb2ac4a08c4caada2d5985c1a422f5a9b168828
Yjm <19307130191@fudan.edu.cn>
2021-09-08 12:25:47 +0800

    committed

---
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -0,0 +1,26 @@
+package hw3.hash;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OomageTestUtility {
+    public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
+        /*
+         * Write a utility function that returns true if the given oomages
+         * have hashCodes that would distribute them fairly evenly across
+         * M buckets. To do this, convert each oomage's hashcode in the
+         * same way as in the visualizer, i.e. (& 0x7FFFFFFF) % M.
+         * and ensure that no bucket has fewer than N / 50
+         * Oomages and no bucket has more than N / 2.5 Oomages.
+         */
+        int N = oomages.size();
+        int[] buckets = new int[M];
+        for (Oomage oomage : oomages) {
+            int bucketNum = (oomage.hashCode() & 0x7FFFFFFF) % M;
+            buckets[bucketNum]++;
+        }
+        int small = Arrays.stream(buckets).min().getAsInt();
+        int big = Arrays.stream(buckets).max().getAsInt();
+        return (big < N / 2.5) && (small > N / 50);
+    }
+}
